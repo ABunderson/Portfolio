ee00652fb9e10937ce5cd80ecb57045040e89d20
Adrienne Bunderson <adriennebunderson@gmail.com>
2025-04-23 16:03:17 -0600

    Fix build

--- src/app/(other)/project/[id]/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProjectLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      {children}
-      <h2>Featured projects section</h2>
-    </div>
-  );
-}
