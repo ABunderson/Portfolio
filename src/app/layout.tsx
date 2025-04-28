427085caff2d94680d72bef809c0bdc528388767
Adrienne Bunderson <adriennebunderson@gmail.com>
2025-04-28 14:40:00 -0600

    Add favicon and remove hi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Portfolio Adrienne Bunderson',
+  title: 'Adrienne Bunderson Portfolio',
   description: 'A porfolio showcasing the work of Adrienne Bunderson',
 };
 
